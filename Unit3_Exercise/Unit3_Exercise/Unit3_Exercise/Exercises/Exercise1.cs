0ac5c12d1adbf97fdefa6427e0a6d6f33ee9d482
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-23 12:25:42 +0100

    Exercise 2

---
+++ Unit3_Exercise/Unit3_Exercise/Unit3_Exercise/Exercises/Exercise1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3_Exercise.Exercises
+{
+    public class Exercise1
+    {
+
+        int firstVariable;
+        int secondVariable;
+
+        //Class builder
+       public Exercise1(int firstVariable, int secondVariable)
+        {
+            this.firstVariable = firstVariable;
+            this.secondVariable = secondVariable;
+        }
+
+        ///Method that calculates and returns the result of exercise 1.
+        
+         public void ExerciseResult()
+        {
+
+            Console.WriteLine("FirstVariable is equal to: {0}", firstVariable);
+            Console.WriteLine("SecondVariable is equal to: {0}", secondVariable);
+
+            Console.WriteLine("FirstVariable plus SecondVariable are equal to: {0}", firstVariable + secondVariable);
+            Console.WriteLine("FirstVariable less SecondVariable are equal to: {0}", firstVariable - secondVariable);
+            Console.WriteLine("FirstVariable multiplied SecondVariable are equal to: {0}", firstVariable * secondVariable);
+
+            //If the secondVariable is zero it can not be divided by zero.
+            if (secondVariable != 0)
+            {
+                Console.WriteLine("FirstVariable divided SecondVariable are equal to: {0}", firstVariable / secondVariable);
+
+                Console.WriteLine("FirstVariable divided SecondVariable are equal to: {0}", firstVariable % secondVariable);
+            }
+            else
+            {
+                Console.WriteLine("Please Sir, do not divide by zero. Thank you.");
+            }
+           
+           
+            
+        }
+    }
+}
---
+++ Unit3_Exercise/Unit3_Exercise/Unit3_Exercise/Exercises/Exercise2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3_Exercise.Exercises
+{
+    
+    public class Exercise2
+    {
+
+        int N;
+        double A;
+        char C;
+
+        //Class builder
+        public Exercise2(int N, double A, char C)
+        {
+            this.N = N;
+            this.A = A;
+            this.C = C;
+        }
+
+        ///Method that calculates and returns the result of exercise 2.
+
+        public void ExerciseResult()
+        {
+            Console.WriteLine("N is equal to: {0}", N);
+            Console.WriteLine("A is equal to: {0}", A);
+            Console.WriteLine("C is equal to: {0}", C);
+
+
+            Console.WriteLine("N plus A are equal to: {0}", N + A);
+            Console.WriteLine("A less N is equal to: {0}", A - N);
+            Console.WriteLine("The ASCII table value for C is: {0}", Convert.ToInt32(C));
+
+
+        }
+    }
+}
