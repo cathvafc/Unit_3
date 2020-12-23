7962aad02868d92830c924859c68b920847d3ed1
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-23 12:27:26 +0100

    Exercise 3

---
+++ Unit3_Exercise/Unit3_Exercise/Unit3_Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Unit3_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Exercise 1
+            //Unit3_Exercise.Exercises.Exercise1 firstExercise = new Exercises.Exercise1(10,4);
+
+            ////Method that shows the exercises results
+            //firstExercise.ExerciseResult();
+
+            ////Exercise 2
+            //Unit3_Exercise.Exercises.Exercise2 secondExercise = new Exercises.Exercise2(10, 4.2, 'G');
+
+            ////Method that shows the exercises results
+            //secondExercise.ExerciseResult();
+
+            //Exercise 3
+            Unit3_Exercise.Exercises.Exercise3 thirdExercise = new Exercises.Exercise3(10, 5, 3.2, 1.6);
+
+            //Method that shows the exercises results
+            thirdExercise.ExerciseResult();
+
+            ////Exercise 4
+            //Unit3_Exercise.Exercises.Exercise4 forthExercise = new Exercises.Exercise4(1);
+
+            ////Method that shows the exercises results
+            //forthExercise.ExerciseResult();
+
+            ////Exercise 5
+            //Unit3_Exercise.Exercises.Exercise5 fifthExercise = new Exercises.Exercise5(2,4,6,8);
+
+            ////Method that shows the exercises results
+            //fifthExercise.ExerciseResult();
+        }
+    }
+}
