ec92e0cc793bf54b75e88ecfc5a5c82db8353e92
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-23 12:31:32 +0100

    All exercises

---
+++ Unit3_Exercise/Unit3_Exercise/Exercises/Exercise4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3_Exercise.Exercises
+{
+    public class Exercise4
+    {
+
+        int N;
+        //Class builder
+        public Exercise4(int N)
+        {
+            this.N = N;
+        }
+
+        ///Method that calculates and returns the result of exercise 4.
+
+        public void ExerciseResult()
+        {
+            N += 77;
+            Console.WriteLine("N + 77 is: {0}", N);
+
+            N -= 3;
+            Console.WriteLine("N - 3 is: {0}", N);
+
+            N *= 2;
+            Console.WriteLine("N * 2 is: {0}", N);
+        }
+    }
+}
---
+++ Unit3_Exercise/Unit3_Exercise/Exercises/Exercise5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit3_Exercise.Exercises
+{
+    public class Exercise5
+    {
+
+        int A;
+        int B;
+        int C;
+        int D;
+        //Class builder
+        public Exercise5(int A, int B, int C, int D)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.D = D;
+        }
+
+        ///Method that calculates and returns the result of exercise 5.
+
+        public void ExerciseResult()
+        {
+
+            B = C;
+            Console.WriteLine("B is equal to: {0}", B);
+
+            C = A;
+            Console.WriteLine("C is equal to: {0}", C);
+
+            A = D;
+            Console.WriteLine("A is equal to: {0}", A);
+
+            D = B; //Takes the new value of B.
+            Console.WriteLine("B is equal to: {0}", D);
+
+        }
+    }
+}
